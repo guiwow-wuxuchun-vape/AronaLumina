71b7314be1fafd915512fc8fcb9676588cc2f365
稽静小白 <159681919+guiwow-wuxuchun-vape@users.noreply.github.com>
2025-09-07 11:22:57 +0800

    更新 LauncherCard.kt

--- app/src/main/java/com/project/lumina/client/router/launch/LauncherCard.kt
+++ app/src/main/java/com/project/lumina/client/router/launch/LauncherCard.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun LauncherCard(
